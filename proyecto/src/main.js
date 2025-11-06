2e30249bf751b9c38c83e5051b2a199a843010bd
ronald4992 <ronald.lopezc@uniagustiniana.edu.co>
2025-11-06 20:35:56 +0000

    second commit

--- proyecto/src/main.js
+++ proyecto/src/main.js
@@ -1,8 +1,9 @@
 import { mostrarRegistro } from './register.js';
 import { mostrarLogin } from './login.js';
 import { mostrarMVP } from './mvp.js';
+import { mostrarUser } from './user.js';
 // ✅ Mostrar la página de registro al iniciar la app
 
 
 
-mostrarLogin();
+mostrarUser();
